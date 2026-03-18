715015156a4edb075001b03b98aa8c4b11c5c0fb
Euan-Benson <euanbson@gmail.com>
2026-03-18 11:46:22 +0000

    add event listeners

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,7 +1,24 @@
+// #region Objects
 const pet = {
     energy: 50,
 
     rest(){
         this.energy += 10;
+        console.log(this.energy);
     },
 }
+// #endregion
+
+// #region Functions
+function handleRestClick(){
+    pet.rest();
+}
+// #endregion
+
+// #region Variables
+const restButton = document.getElementById("rest");
+// #endregion
+
+// #region Event Listeners
+ restButton.addEventListener("click", handleRestClick);
+// #endregion
